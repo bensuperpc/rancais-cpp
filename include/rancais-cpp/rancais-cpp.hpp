3061e8f3b0397b30322cd73ca5490d570d0171cc
Bensuperpc <bensuperpc@gmail.com>
2022-10-27 18:59:55 +0200

    WIP on lib
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- include/rancais-cpp/rancais-cpp.hpp
+++ include/rancais-cpp/rancais-cpp.hpp
@@ -1,5 +1,61 @@
 #pragma once
 
+/*
+  Use preprocessor to replace/define new keywords instead of using/typedef to avoid including all c++ libraries :D
+*/
+
+#define entier int
+#define flottant float
+#define caractere char
+
+#define pour for
+#define tant_que while
+#define faire do
+#define pour_chaque for each
+#define dans in
+#define avec using
+
+#define retourne return
+#define cas switch
+#define cas_de case
+#define defaut default
+
+#define si if
+#define sinon else
+#define sinon_si else if
+
+#define vrai true
+#define faux false
+
+#define classe class
+#define publique public
+#define prive private
+#define protege protected
+#define ami friend
+#define virtuel virtual
+
+#define vecteur vector
+#define liste list
+#define file queue
+#define pile stack
+#define chaine_de_caracteres std::string
+
+#define statique static
+#define constante const
+#define volatilee volatile
+
+#define structure struct
+#define unionn union
+
+#define nouveau new
+#define detruire delete
+
+#define essayer try
+#define attraper catch
+#define lancer throw
+
+#define taille size
+
 #include <string>
 
 /**
--- test/source/rancais-cpp_test.cpp
+++ test/source/rancais-cpp_test.cpp
@@ -1,8 +1,15 @@
 #include "rancais-cpp/rancais-cpp.hpp"
 
+#include <iostream>
+
 auto main() -> int
 {
   auto const result = name();
 
   return result == "rancais-cpp" ? 0 : 1;
+
+  pour(auto i = 0; i < 10; ++i)
+  {
+    std::cout << "Bonjour, le monde!" << std::endl;
+  }
 }
